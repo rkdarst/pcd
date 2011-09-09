3cc7ba4eb6f03728f8bed5c295beb9889fd2eca5
gmh2123 <gmh2123@columbia.edu>
2011-09-09 21:41:05 +0300

    amorphous-update
    Ignore-this: d4a0064e92a9cae651083a4bcef23b6c
    energy function added which should allow for detection in 2d amorphous system test. may need modification
    
    darcs-hash:20110909184105-0b7fe-300c70f8a6149bce0cf2d86c53af6c40cf5ea860.gz

--- tests/testamorphous.py
+++ tests/testamorphous.py
@@ -12,107 +12,58 @@ def readgro(gro_file,dimensions=2):
     natoms=int(configuration_lines[1])
     configuration=numpy.array( [ line.split()[3:3+dimensions] for line in configuration_lines[2:2+natoms] ],dtype=float)
     box=[float(x) for x in configuration_lines[-1].split()]
+    atomtypes=[line.split()[1] for line in configuration_lines[2:2+natoms] ]
+    ntypea=atomtypes.index(atomtypes[-1])
     length=box[0]
-    return length, configuration
+    return length,ntypea,configuration
 
-def glencoords():
-    # By Glen Hocky, August 2011
-
-    natoms=64
-    #import pylab
-    #fig=pylab.figure()
-    #ax = fig.add_subplot(111, aspect='equal')
-    positions=[[i,j] for i in range(natoms) for j in range(natoms)]
-    values=[[i,j] for i in range(natoms) for j in range(natoms)]
-    for idx,positions in enumerate(positions):
-        for i in range(1,natoms+2,2):
-            if positions[0] > i: values[idx][0]=values[idx][0]+1
-            if positions[1] > i: values[idx][1]=values[idx][1]+1
-        for i in range(3,natoms+4,4):
-            if positions[0] > i: values[idx][0]=values[idx][0]+2
-            if positions[1] > i: values[idx][1]=values[idx][1]+2
-        for i in range(7,natoms+8,8):
-            if positions[0] > i: values[idx][0]=values[idx][0]+3
-            if positions[1] > i: values[idx][1]=values[idx][1]+3
-        for i in range(15,natoms+16,16):
-            if positions[0] > i: values[idx][0]=values[idx][0]+4
-            if positions[1] > i: values[idx][1]=values[idx][1]+4
-
-    xpoints=[x[0] for x in values]
-    ypoints=[x[1] for x in values]
-    #pylab.scatter(xpoints,ypoints)
-    #pylab.show()
-    return numpy.asarray(values)
-
-def fractalsquare(sep):
-    """Make a fractal square with parameter `sep`
-
-    Number of nodes per side is 2^sep.
-
-    Largest separation between nodes is sep.
-    """
-    if sep == 0:
-        return numpy.asarray([[0,0]]), 1
-    else:
-        sq, L = fractalsquare(sep=sep-1)
-        # Offset it by size of smaller square plus the new spacing
-        offset = L
-        L = L + L
-        new = numpy.concatenate((sq,
-                                 sq+(0, offset),
-                                 sq+(offset, 0),
-                                 sq+(offset, offset),
-                                 ))
-        return new, L+1
-
+def e_softsphere_i(d,sigma,epsilon=1):
+    energy = epsilon*(sigma/d)**12
+    energy += 0.5*epsilon
+    energy *= 1000
+    intn = numpy.round(energy)
+    return intn
 
 def e_lj(d):
     d *= 2**(1/6.)
     energy = 4 * (1/d**12. - 1/d**6.)
+    energy += 0.5
     energy *= 1000
-    energy += 1
-    intn = numpy.round(energy)
-    return intn
-def e_inverse_6(d):
-    energy = -(1./d**6)
-    energy *= 729
-    energy += 10
     intn = numpy.round(energy)
     return intn
 
-
-def set_imatrix(imatrix, coords, periodic=None):
+def get_imatrix(coords,sigmas,ntypea,periodic=None):
+    imatrix=numpy.zeros((len(coords),len(coords)),dtype=int)
     orig_settings = numpy.seterr()
     numpy.seterr(all="ignore")
     for n1 in range(len(coords)):
+        atomtype=int(n1>=ntypea)
         delta = coords[n1] - coords
         if periodic:
             delta -=  numpy.round(delta/float(periodic))*periodic
         delta = delta**2
         dist = numpy.sum(delta, axis=1)
         dist = numpy.sqrt(dist)
-        e = e_lj(dist)
+        sigma=sigmas[atomtype] 
+        e = e_softsphere_i(dist,sigma)
         imatrix[n1, :] = e
     numpy.seterr(**orig_settings)
-
+    return imatrix
 
 testsdir=os.path.dirname(sys.argv[0])
 datadir=os.path.abspath(os.path.join(testsdir,'../data'))
 grofile=os.path.join(datadir,'2dss32_n240_T0.5_1.gro')
-L,coords=readgro(grofile)
+L,ntypea,coords=readgro(grofile)
+ntypeb=len(coords)-ntypea
 
-G = models.Graph.from_coords_and_efunc(coords,
-                                     #lambda x: e_lj(x)*100,
-                                     e_lj,
-                                     periodic=L)
-#G = models.Graph(N=len(coords))
-#set_imatrix(G.imatrix, coords, periodic=L)
+sigma1=numpy.concatenate(( numpy.ones(ntypea),1.1*numpy.ones(ntypeb) ))
+sigma2=numpy.concatenate(( 1.1*numpy.ones(ntypea),1.4*numpy.ones(ntypeb) ))
+sigmas=numpy.array((sigma1,sigma2))
 
+imatrix=get_imatrix(coords,sigmas,ntypea,periodic=L)
 
-#sys.path.append('/home/richard')
-#del sys.path[-1]
-
-#G = G.from_imatrix(imatrix)
+G = models.Graph(N=len(coords))
+G = G.from_imatrix(imatrix)
 
 G.minimize(gamma=0)
 G.savefig('img.png', coords=coords)
