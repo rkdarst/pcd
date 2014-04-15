e7b8379aef201a36dd7feb1e8955b7d49778484d
Richard Darst <rkd@zgib.net>
2014-04-15 20:28:21 +0300

    grow: Add generalized class and HolmeGraph and SquareClosure

--- grow.py
+++ grow.py
@@ -10,7 +10,22 @@ import random
 
 import pcd.util
 
-class GrowFitness(object):
+class GrowingGraph(object):
+    def grow(self, N):
+        """Add enough nodes to graph to reach N nodes."""
+        while len(self.g) < N:
+            #print len(self.g)
+            self.add()
+
+    @classmethod
+    def get(cls, **kwargs):
+        N = kwargs.pop('N')
+        grower = cls(**kwargs)
+        grower.grow(N)
+        return grower.g
+
+
+class GrowFitness(GrowingGraph):
     """Growing graph model, with power-law attachment
 
     neighbor_mode: how to add edges beyond the second
@@ -142,11 +157,144 @@ class GrowFitness(object):
                 g.node[n0]['edge_order'].append(n_next)
 
 
-    def grow(self, N):
-        """Add enough nodes to graph to reach N nodes."""
-        while len(self.g) < N:
-            #print len(self.g)
-            self.add()
+class HolmeGraph(GrowingGraph):
+    def __init__(self, m, mt=None, Pt=None, m0=3, seed=None):
+        """
+
+        m: int
+            additional edges for each successive node
+        Pt: float
+            Probability of triad formation.
+        mt: float
+            Avg number of clustered edges per run.  Pt is then set to mt/(m-1)
+        m0: int
+             original number of nodes.
+        """
+        self.g = networkx.Graph()
+        self.m = m
+        m0 = max(m0, m+1)
+        self.m0 = m0
+        self.rng = random.Random(seed) # seed not given: random seed.
+        # We can specify eather Pt or Mt
+        if Pt is None:
+            Pt = mt/float(m-1)
+        else:
+            assert mt is None
+        assert Pt <= 1.0, "Pt=%s must be <= 1.0"%Pt
+        self.Pt = Pt
+        # Add initial nodes
+        for n in range(m0):
+            self.g.add_node(n)  # One free unit for each node.
+        # Preferential attachment selector.  Selects random nodes
+        # proportional to degree.
+        self.pa_selector = list(range(m0))
+    def add(self):
+        g = self.g
+        n = len(g)
+        pa_selector = self.pa_selector
+        rng = self.rng
+
+        g.add_node(n)
+        pa_selector.append(n)  # One free unit for each node.
+
+        def pa_select():
+            if len(g) <= len(edges_made):
+                return None
+            while True:
+                n1 = rng.choice(pa_selector)
+                if n1 in edges_made:
+                    continue
+                break
+            return n1
+
+        edges_made = set((n, ))
+        assert len(g) > self.m, "Graph to small and not enough edges to connect to."
+        for e in range(self.m):
+            # Random edge
+            n1 = pa_select()
+            if n1 is None:
+                print "  can't make more edges at %d"%n
+                break
+            assert not g.has_edge(n, n1)
+            assert n != n1
+            g.add_edge(n, n1)
+            edges_made.add(n1)
+            pa_selector.extend((n, n1))
+
+            if rng.uniform(0,1) <= self.Pt:
+                # Next, add clustered edge
+                second_neighbors = set(g.neighbors(n1))
+                available = second_neighbors - edges_made
+                # PA or triad closure step?
+                if available:
+                    # Triad closure
+                    n2 = rng.sample(available, 1)[0]
+                else:
+                    # No second neighbor available.  Do a random PA step
+                    # instead.
+                    n2 = pa_select()
+                    if n2 is None:
+                        print "    can't make more edges at %d, %d"%(n, n1)
+                        continue
+                assert not g.has_edge(n, n2)
+                assert n != n2
+                assert n1 != n2
+
+                g.add_edge(n, n2)
+                edges_made.add(n2)
+                pa_selector.extend((n, n2))
+
+
+class SquareClosure(GrowingGraph):
+    def __init__(self, ns, seed=None, m0=3):
+        self.g = networkx.Graph()
+        self.ns = ns
+        self.rng = random.Random(seed) # seed not given: random seed.
+        for n in range(m0):
+            self.g.add_node(n)
+            for n1 in range(n):
+                self.g.add_edge(n, n1)
+    def add(self):
+        g = self.g
+        rng = self.rng
+        n = len(g)
+        g.add_node(n)
+
+        # Random base of new attachment
+        n1 = random.randint(0, len(g)-2)  # exclude endpoint and n
+        #print 'graph status:', len(g), g.number_of_edges(), n, n1#, g.nodes()
+        g.add_edge(n, n1)
+
+        inner_nodes = set((n, n1))
+        shell_nodes = inner_nodes
+        for dist, n_at_dist in enumerate(self.ns):
+            dist += 1  # dist starts from 1, not zero
+            nodes_available = set.union(*(set(g.neighbors(_))
+                                          for _ in shell_nodes))
+            #print '  nodes_available:', nodes_available
+            #print '  inner_nodes:', inner_nodes
+            shell_nodes = nodes_available - inner_nodes
+            #print '  shell_nodes:', shell_nodes
+            #print ' ', dist, n_at_dist, len(inner_nodes), len(nodes_available)
+            if len(shell_nodes) == 0:
+                # We can never add any more from now on.
+                break
+
+            n_at_dist = min(n_at_dist, len(shell_nodes))
+            if n_at_dist == 0:
+                inner_nodes |= shell_nodes
+                continue
+
+            new_edges = rng.sample(shell_nodes, n_at_dist)
+            #print '  new edges:', new_edges, len(shell_nodes)
+            for n2 in new_edges:
+                #print '    linking to:', n2
+                if n2 == n or n2 == n1: raise
+                assert not g.has_edge(n, n2)
+                g.add_edge(n, n2)
+
+            inner_nodes |= nodes_available
+
 
 
 def growsf_gb(N, p, beta, kappa, m=2, **kwargs):
